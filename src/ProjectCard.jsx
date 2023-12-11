24f40d3ee986937012b76006580e5cd1b231718d
Mahbubullah <mahbubullahpathan@gmail.com>
2023-12-12 03:33:38 +0600

    projects

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
 const ProjectCard = ({ project }) => {
-    console.log(project?.view);
+  console.log(project?.view);
 
-
-    const openInNewTab = (url) => {
-        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
-        if (newWindow) newWindow.opener = null
-      }
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
   return (
     <div>
-      <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+      <div className="group relative rounded-lg cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
         <div className="h-96">
           <img
             className="h-full w-full object-cover transition-transform duration-500 "
@@ -24,23 +23,27 @@ const ProjectCard = ({ project }) => {
             <h1 className="w-full bg-amber-500 py-1 font-dmserif text-3xl font-bold text-black">
               {project?.name}
             </h1>
-            <p className="mb-3 p-2 text-left text-lg italic text-black opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            <p className="mb-3 p-2 text-left text-lg  font-medium text-black opacity-0 transition-opacity duration-300 group-hover:opacity-100">
               {project?.desc}
             </p>
             <div className="grid grid-cols-3 gap-2  mx-2">
               <button className="rounded-full bg-neutral-900 px-3.5 py-2 font-com text-sm capitalize text-white shadow shadow-black/60">
                 Details
               </button>
-            
-                <button onClick={()=> openInNewTab(project?.links?.view)} className="rounded-full bg-neutral-900 px-3.5 py-2 font-com text-sm capitalize text-white shadow shadow-black/60">
-                 Live Link
-                </button>
-             
-             
-              <button onClick={()=> openInNewTab(project?.links?.code)} className="rounded-full bg-neutral-900 px-3.5 py-2 font-com text-sm capitalize text-white shadow shadow-black/60">
+
+              <button
+                onClick={() => openInNewTab(project?.links?.view)}
+                className="rounded-full bg-neutral-900 px-3.5 py-2 font-com text-sm capitalize text-white shadow shadow-black/60"
+              >
+                Live Link
+              </button>
+
+              <button
+                onClick={() => openInNewTab(project?.links?.code)}
+                className="rounded-full bg-neutral-900 px-3.5 py-2 font-com text-sm capitalize text-white shadow shadow-black/60"
+              >
                 Code
               </button>
-             
             </div>
           </div>
         </div>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
 import ProjectCard from "./ProjectCard";
-
+import { FaLaptopCode } from "react-icons/fa";
 
 const Projects = () => {
-
-    const [projets, setProjects] = useState([]);
+  const [projets, setProjects] = useState([]);
 
   useEffect(() => {
     fetch("../public/projects.json")
@@ -13,24 +12,22 @@ const Projects = () => {
       .then((data) => setProjects(data));
   }, []);
   console.log(projets);
-    return (
-        <div className="p-10 bg-gradient-to-b from-[#000031] to-[#00002c]">
+  return (
+    <div className="px-10 pt-10 pb-20 bg-gradient-to-b from-[#000031] to-[#00002c]">
+      <div className="text-white text-center text-3xl font-bold mb-8">
+        <h2 className="flex items-center justify-center gap-2">
+          {" "}
+          <FaLaptopCode /> My <span className="text-[#ffd900]">Projects</span>
+        </h2>
+      </div>
 
-            <div className="text-white text-center text-3xl font-bold">
-                <h2> My Projects</h2>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-
-                {
-                    projets?.map((project, idx) => <ProjectCard key={idx} project={project}/> )
-                }
-            </div>
-
-
-            
-        </div>
-    );
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {projets?.map((project, idx) => (
+          <ProjectCard key={idx} project={project} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
