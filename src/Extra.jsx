dd23bcbf343dee6d844b0db07d31001f771d6c3e
Mahbubullah <mahbubullahpathan@gmail.com>
2023-12-12 13:05:34 +0600

    extra curriculum activities

--- src/Extra.jsx
+++ src/Extra.jsx
@@ -1,15 +1,213 @@
-import { MdOutlineWork } from "react-icons/md";
+import { MdOutlineWork, MdWorkHistory } from "react-icons/md";
 
 const Extra = () => {
-    return (
-        <div className="py-10 max-w-4xl mx-auto">
-
-            <div className="text-4xl  font-bold">
-                <h2 className="flex items-center justify-center gap-2"><MdOutlineWork />Extra Curriculum Activities</h2>
+  return (
+    <div className="py-10 max-w-4xl mx-auto">
+      <div className="text-4xl  font-bold">
+        <h2 className="flex items-center justify-center gap-2">
+          <MdOutlineWork />
+          Extra Curriculum Activities
+        </h2>
+      </div>
+      <div className="hidden md:block mt-6">
+        <ul className="timeline timeline-vertical">
+          <li>
+            <div className="timeline-start rounded-lg p-4 bg-[#f68c09] w-96">
+              <div className="text-xl font-bold">
+                <h2>Green University Computer Club</h2>
+              </div>
+              <div className=" ">
+                <h3 className="font-bold text-base">
+                  Joint Publication Secretary
+                </h3>
+                <p className="text-sm font-normal">March 2022 - August 2023</p>
+              </div>
             </div>
-            
-        </div>
-    );
+            <div className="timeline-middle">
+              <div className="text-2xl text-[#020133] rounded-full border-amber-500 border-4  p-1 ">
+                <MdWorkHistory />
+              </div>
+            </div>
+            <hr className="bg-[#020133]" />
+          </li>
+          <li>
+            <hr className="bg-[#020133]" />
+            <div className="timeline-middle">
+              <div className="text-2xl text-[#020133] rounded-full border-amber-500 border-4  p-1 ">
+                <MdWorkHistory />
+              </div>
+            </div>
+            <div className="timeline-end rounded-lg p-4 bg-[#f68c09] w-96">
+              <div className="text-xl font-bold">
+                <h2>2022 ICPC Asia Dhaka Regional.</h2>
+              </div>
+              <div className=" ">
+                <h3 className="font-bold text-base">
+                 Volunteer
+                </h3>
+                <p className="text-sm font-normal">Nov 2022 - March 2023</p>
+              </div>
+              </div>
+            <hr className="bg-[#020133]" />
+          </li>
+          <li>
+            <hr className="bg-[#020133]" />
+            <div className="timeline-start rounded-lg p-4 bg-[#f68c09] w-96">
+              <div className="text-xl font-bold">
+                <h2>GUVI, IIT-M & IIM-A incubated Ed-tech company</h2>
+              </div>
+              <div className=" ">
+                <h3 className="font-bold text-base">
+                Campus Ambassador
+                </h3>
+                <p className="text-sm font-normal">September 2022 - November 2023</p>
+              </div>
+            </div>
+            <div className="timeline-middle">
+              <div className="text-2xl text-[#020133] rounded-full border-amber-500 border-4  p-1 ">
+                <MdWorkHistory />
+              </div>
+            </div>
+            <hr className="bg-[#020133]" />
+          </li>
+          <li>
+            <hr className="bg-[#020133]" />
+            <div className="timeline-middle">
+              <div className="text-2xl text-[#020133] rounded-full border-amber-500 border-4  p-1 ">
+                <MdWorkHistory />
+              </div>
+            </div>
+            <div className="timeline-end rounded-lg p-4 bg-[#f68c09] w-96">
+              <div className="text-xl font-bold">
+                <h2>SUST SWE Technovent</h2>
+              </div>
+              <div className=" ">
+                <h3 className="font-bold text-base">
+                Campus Ambassador
+                </h3>
+                <p className="text-sm font-normal"> 2023</p>
+              </div>
+            </div>
+            <hr className="bg-[#020133]" />
+          </li>
+          <li>
+            <hr className="bg-[#020133]" />
+            <div className="timeline-start rounded-lg p-4 bg-[#f68c09] w-96">
+              <div className="text-xl font-bold">
+                <h2>Strengthening Leadership Skill for tht Club Leaders.</h2>
+              </div>
+              <div className=" ">
+                <h3 className="font-bold text-base">
+                 Awarded by Green University of Bangladesh
+                </h3>
+                <p className="text-sm font-normal">November 2022 </p>
+              </div>
+            </div>
+            <div className="timeline-middle">
+              <div className="text-2xl text-[#020133] rounded-full border-amber-500 border-4  p-1 ">
+                <MdWorkHistory />
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mx-6 mt-14 md:hidden">
+        <ol className="relative border-s-4 border-[#020133] ">
+          <li className="mb-10 ms-6">
+            <span className="absolute flex items-center justify-center w-6 h-6  rounded-full -start-3 ring-8 ">
+              <div className="text-2xl text-[#020133] rounded-full border-amber-500 border-4  p-1 ">
+                <MdWorkHistory />
+              </div>
+            </span>
+            <div className="timeline-start rounded-lg p-4 bg-[#f68c09] w-full">
+              <div className="text-xl font-bold">
+                <h2>Strengthening Leadership Skill for tht Club Leaders.</h2>
+              </div>
+              <div className=" ">
+                <h3 className="font-bold text-base">
+                 Awarded by Green University of Bangladesh
+                </h3>
+                <p className="text-sm font-normal">November 2022 </p>
+              </div>
+            </div>
+          </li>
+          <li className="mb-10 ms-6">
+            <span className="absolute flex items-center justify-center w-6 h-6  rounded-full -start-3 ring-8 ">
+              <div className="text-2xl text-[#020133] rounded-full border-amber-500 border-4  p-1 ">
+                <MdWorkHistory />
+              </div>
+            </span>
+            <div className="timeline-end rounded-lg p-4 bg-[#f68c09] ">
+              <div className="text-xl font-bold">
+                <h2>2022 ICPC Asia Dhaka Regional.</h2>
+              </div>
+              <div className=" ">
+                <h3 className="font-bold text-base">
+                 Volunteer
+                </h3>
+                <p className="text-sm font-normal">Nov 2022 - March 2023</p>
+              </div>
+              </div>
+          </li>
+          <li className="ms-6 mb-6">
+            <span className="absolute flex items-center justify-center w-6 h-6  rounded-full -start-3 ring-8 ">
+              <div className="text-2xl text-[#020133] rounded-full border-amber-500 border-4  p-1 ">
+                <MdWorkHistory />
+              </div>
+            </span>
+            <div className="timeline-start rounded-lg p-4 bg-[#f68c09] ">
+              <div className="text-xl font-bold">
+                <h2>GUVI, IIT-M & IIM-A incubated Ed-tech company</h2>
+              </div>
+              <div className=" ">
+                <h3 className="font-bold text-base">
+                Campus Ambassador
+                </h3>
+                <p className="text-sm font-normal">September 2022 - November 2023</p>
+              </div>
+            </div>
+          </li>
+          <li className="mb-10 ms-6">
+            <span className="absolute flex items-center justify-center w-6 h-6  rounded-full -start-3 ring-8 ">
+              <div className="text-2xl text-[#020133] rounded-full border-amber-500 border-4  p-1 ">
+                <MdWorkHistory />
+              </div>
+            </span>
+            <div className="timeline-end rounded-lg p-4 bg-[#f68c09] ">
+              <div className="text-xl font-bold">
+                <h2>SUST SWE Technovent</h2>
+              </div>
+              <div className=" ">
+                <h3 className="font-bold text-base">
+                Campus Ambassador
+                </h3>
+                <p className="text-sm font-normal"> 2023</p>
+              </div>
+            </div>
+          </li>
+          <li className="mb-10 ms-6">
+            <span className="absolute flex items-center justify-center w-6 h-6  rounded-full -start-3 ring-8 ">
+              <div className="text-2xl text-[#020133] rounded-full border-amber-500 border-4  p-1 ">
+                <MdWorkHistory />
+              </div>
+            </span>
+            <div className="timeline-start rounded-lg p-4 bg-[#f68c09]">
+              <div className="text-xl font-bold">
+                <h2>Strengthening Leadership Skill for tht Club Leaders.</h2>
+              </div>
+              <div className=" ">
+                <h3 className="font-bold text-base">
+                 Awarded by Green University of Bangladesh
+                </h3>
+                <p className="text-sm font-normal">November 2022 </p>
+              </div>
+            </div>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 };
 
 export default Extra;
