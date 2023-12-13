ea953bde307f763ccb2fc6db0ce018baeb2b5b32
Mahbubullah <mahbubullahpathan@gmail.com>
2023-12-14 05:40:55 +0600

    projects details

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { MdWorkspacePremium } from "react-icons/md";
 
 const ProjectCard = ({ project }) => {
   console.log(project?.view);
@@ -26,10 +26,77 @@ const ProjectCard = ({ project }) => {
             <p className="mb-3 p-2 text-left text-lg  font-medium text-black opacity-0 transition-opacity duration-300 group-hover:opacity-100">
               {project?.desc}
             </p>
-            <div className="grid grid-cols-2 gap-8  mx-2">
-              {/* <button className="rounded-full bg-neutral-900 px-3.5 py-2 font-com text-sm capitalize text-white shadow shadow-black/60">
+            <div className="grid grid-cols-3 gap-2  mx-2">
+              <button
+                onClick={() =>
+                  document.getElementById("my_modal_5").showModal()
+                }
+                className="rounded-full bg-neutral-900 px-3.5 py-2 font-com text-sm capitalize text-white shadow shadow-black/60"
+              >
                 Details
-              </button> */}
+              </button>
+              {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+              <dialog
+                id="my_modal_5"
+                className="modal  modal-bottom sm:modal-middle"
+              >
+                <div className="modal-box relative">
+                  <div className="container mx-auto ">
+                    <div>
+                      <div className="bg-white  shadow rounded-lg mx-auto">
+                        <div className="">
+                          <h1 className="font-bold text-center text-3xl text-gray-900">
+                            {project?.name}
+                          </h1>
+                          <p className="text-center text-sm text-gray-400 font-medium">
+                            
+                          </p>
+                          <p>
+                            <span></span>
+                          </p>
+                         
+                          <div className="mt-10">
+
+                            <h2>Technologies used</h2>
+                            <hr />
+                          </div>
+                          <div className="flex flex-wrap gap-4 items-center my-5 px-6">
+                            {project?.Technology?.map((data, idx) => (
+                              <h2
+                                className="bg-[#272770] text-white text-base font-medium py-2 px-4 rounded-full"
+                                key={idx}
+                              >
+                                {data}
+                              </h2>
+                            ))}
+                          </div>
+                         
+                          <div className="mt-10">
+                            <h2>Features</h2>
+                            <hr />
+                            <div className="text-left">
+                            {project?.Feature?.map((data, idx) => (
+                               <h2 key={idx} className="text-lg font-semibold flex items-center gap-4"><MdWorkspacePremium /> 
+                                {data}
+                              </h2>
+                            ))}
+                            
+
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="modal-action">
+                    <form method="dialog">
+                      {/* if there is a button in form, it will close the modal */}
+                      <button className="btn">Close</button>
+                    </form>
+                  </div>
+                </div>
+              </dialog>
 
               <button
                 onClick={() => openInNewTab(project?.links?.view)}
