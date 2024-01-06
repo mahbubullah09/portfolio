61a670dc00442139e174bfe5db3cc3f6f71cc573
Mahbubullah <mahbubullahpathan@gmail.com>
2024-01-06 15:57:44 +0600

    details functionality implement

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import { MdWorkspacePremium } from "react-icons/md";
 
-const ProjectCard = ({ project }) => {
+const ProjectCard = ({ project, setDetails }) => {
   console.log(project?.view);
 
   const openInNewTab = (url) => {
@@ -27,75 +27,18 @@ const ProjectCard = ({ project }) => {
               {project?.desc}
             </p>
             <div className="grid grid-cols-3 gap-2  mx-2">
-              {/* <button
-                onClick={() =>
-                  document.getElementById("my_modal_5").showModal()
-                }
+              <button
+                onClick={() =>{
+                  document.getElementById("my_modal_5").showModal();
+                  setDetails(project)
+  }}
                 className="rounded-full bg-neutral-900 px-3.5 py-2 font-com text-sm capitalize text-white shadow shadow-black/60"
               >
                 Details
               </button>
               
 
-              <dialog
-                id="my_modal_5"
-                className="modal  modal-bottom sm:modal-middle"
-              >
-                <div className="modal-box relative">
-                  <div className="container mx-auto ">
-                    <div>
-                      <div className="bg-white  shadow rounded-lg mx-auto">
-                        <div className="">
-                          <h1 className="font-bold text-center text-3xl text-gray-900">
-                            {project?.name}
-                          </h1>
-                          <p className="text-center text-sm text-gray-400 font-medium"></p>
-                          <p>
-                            <span></span>
-                          </p>
-
-                          <div className="mt-10">
-                            <h2>Technologies used</h2>
-                            <hr />
-                          </div>
-                          <div className="flex flex-wrap gap-4 items-center my-5 px-6">
-                            {project?.Technology?.map((data, idx) => (
-                              <h2
-                                className="bg-[#272770] text-white text-base font-medium py-2 px-4 rounded-full"
-                                key={idx}
-                              >
-                                {data}
-                              </h2>
-                            ))}
-                          </div>
-
-                          <div className="mt-10">
-                            <h2>Features</h2>
-                            <hr />
-                            <div className="text-left">
-                              {project?.Feature?.map((data, idx) => (
-                                <h2
-                                  key={idx}
-                                  className="text-lg font-semibold flex items-center gap-4"
-                                >
-                                  <MdWorkspacePremium />
-                                  {data}
-                                </h2>
-                              ))}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="modal-action">
-                    <form method="dialog">
-                     
-                      <button className="btn">Close</button>
-                    </form>
-                  </div>
-                </div>
-              </dialog> */}
+              
 
               <button
                 onClick={() => openInNewTab(project?.links?.view)}
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import ProjectCard from "./ProjectCard";
 import { FaLaptopCode } from "react-icons/fa";
+import { MdWorkspacePremium } from "react-icons/md";
 
 const Projects = () => {
   const [projets, setProjects] = useState([]);
+  const [details, setDetails] = useState(null);
 
   useEffect(() => {
     fetch("projects.json")
@@ -23,9 +25,64 @@ const Projects = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {projets?.map((project, idx) => (
-          <ProjectCard key={idx} project={project} />
+          <ProjectCard key={idx} project={project} setDetails={setDetails} />
         ))}
       </div>
+      <dialog id="my_modal_5" className="modal  modal-bottom sm:modal-middle">
+        <div className="modal-box relative">
+          <div className="container mx-auto ">
+            <div>
+              <div className="bg-white  shadow rounded-lg mx-auto">
+                <div className="">
+                  <h1 className="font-bold text-center text-3xl text-gray-900">
+                    {details?.name}
+                  </h1>
+                  <p className="text-center text-sm text-gray-400 font-medium"></p>
+                  <p>
+                    <span></span>
+                  </p>
+
+                  <div className="mt-10">
+                    <h2>Technologies used</h2>
+                    <hr />
+                  </div>
+                  <div className="flex flex-wrap gap-4 items-center my-5 px-6">
+                    {details?.Technology?.map((data, idx) => (
+                      <h2
+                        className="bg-[#272770] text-white text-base font-medium py-2 px-4 rounded-full"
+                        key={idx}
+                      >
+                        {data}
+                      </h2>
+                    ))}
+                  </div>
+
+                  <div className="mt-10">
+                    <h2>Features</h2>
+                    <hr />
+                    <div className="text-left">
+                      {details?.Feature?.map((data, idx) => (
+                        <h2
+                          key={idx}
+                          className="text-lg font-semibold flex items-center gap-4"
+                        >
+                          <MdWorkspacePremium />
+                          {data}
+                        </h2>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
